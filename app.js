d405c5d0dab744044b685fef558e811056dba451
virmontor <virmontor@gmail.com>
2017-11-30 10:37:04 -0600

    subiendo ejercicio

---
+++ app.js
@@ -0,0 +1,18 @@
+var boton = document.getElementById("btn");
+
+function showMenu() {
+  var menu = document.getElementById("opc-menu");
+
+  if (menu.classList.contains("disabled-menu")) {
+    menu.classList.remove("disabled-menu");
+    menu.classList.add("enabled-menu");
+  }
+
+  else {
+    menu.classList.remove("enabled-menu");
+    menu.classList.add("disabled-menu");
+  }
+
+}
+
+boton.addEventListener("click", showMenu);
